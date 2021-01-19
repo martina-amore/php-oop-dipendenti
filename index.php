601be0e6fbe5f41ef1161038f493d670be876dcd
Martina Amore <martina.amore02@gmail.com>
2021-01-19 19:23:04 +0100

    Completo + eccezione

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 
 require_once('classi.php');
 
+$impiegato_salariato = new ImpiegatoSalariato("Mario", "Rossi", "xxxxx", "0124", 80, 25);
+$impiegato_a_ore = new ImpiegatoAOre("Pippo", "Disney", "alkdfleger", "7156", 160, 10);
+echo "Impiegato salariato" . "<br>";
+echo $impiegato_salariato->to_string() . "<br>" . $impiegato_salariato->calcola_compenso() . "<br>";
+echo "Impiegato a ore" . "<br>";
+echo $impiegato_a_ore->to_string() . "<br>" . $impiegato_a_ore->calcola_compenso() . "<br>";
+
 try {
-    $impiegato_salariato = new ImpiegatoSalariato("Mario", "Rossi", "xxxxx", "0124", 80, 25);
-    $impiegato_a_ore = new ImpiegatoAOre("Pippo", "Disney", "alkdfleger", "7156", 160, 10);
-    $impiegato_su_commissione = new ImpiegatoSuCommissione("Pluto", "Disney", "plfkwwna", "FunnyDisney(cancella questo campo per provare l'eccezione)", 1500);
-    echo "Impiegato salariato" . "<br>";
-    echo $impiegato_salariato->to_string() . "<br>" . $impiegato_salariato->calcola_compenso() . "<br>";
-    echo "Impiegato a ore" . "<br>";
-    echo $impiegato_a_ore->to_string() . "<br>" . $impiegato_a_ore->calcola_compenso() . "<br>";
     echo "Impiegato su commissione" . "<br>";
+    $impiegato_su_commissione = new ImpiegatoSuCommissione("Pluto", "Disney", "plfkwwna", "FunnyDisney(cancella questo campo per provare l'eccezione)", 1500);
     echo $impiegato_su_commissione->to_string() . "<br>" . $impiegato_su_commissione->calcola_compenso() . "<br>";
 }
 catch (Exception $e) {
